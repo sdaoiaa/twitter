7cd4104d7a76a87942d016cdf1338589af14aaba
iawivanrodriguezporto <1hiawrodriguezportoivan@gmail.com>
2022-03-27 21:40:00 +0200

    twitt

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+	<h1 style="text-align:center;">Twitter</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script>
+import Header from '../components/Header.vue'
+import Text from '../components/Text.vue'
+
+export default {
+  components : {
+    Text,
+    Header
+  }
+}
+</script>
+
+<template>
+  <Header></Header>
+  <Text></Text>
+</template>
